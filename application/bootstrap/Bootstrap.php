6a4059b31c0b3da5e5588dc9912bff2666df7522
jpcarreira <joaocarreira@me.com>
2014-09-27 18:20:09 +0100

    enabled Zend_Logger and database profiler to Bootstrap

--- application/bootstrap/Bootstrap.php
+++ application/bootstrap/Bootstrap.php
@@ -4,11 +4,38 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
     public $frontController;
 
+
+    protected function _initLogging()
+    {
+        // logging bootstrap resource depends on the front controller
+        $this->bootstrap('frontController');
+
+        // creating an instance of Zend_Log with set up of different environments
+        $logger = new Zend_Log();
+        $writer = 'production' == $this->getEnvironment() ?
+            new Zend_Log_Writer_Stream(APPLICATION_PATH . '/../data/logs/app.log') :
+            new Zend_Log_Writer_Firebug();
+        $logger->addWriter($writer);
+
+        // filtering for critial issues only under production environment
+        if('production' == $this->getEnvironment())
+        {
+            $filter = new Zend_Log_Filter_Priority(Zend_Log::CRIT);
+            $logger->addFilter($filter);
+        }
+
+        // assigning the logger to a propected property of Bootstrap
+        $this->_logger = $logger;
+        Zend_Registry::set('log', $logger);
+    }
+
+
     protected function _initLocale()
     {
         $locale = new Zend_Locale('en_GB');
         Zend_Registry::set('Zend_Locate', $locale);
     }
+
 
     protected function _initViewSettings()
     {
@@ -53,4 +80,20 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
     }
 
 
+    protected function __initDbProfiler()
+    {
+        $this->_logger->info('Bootstrap ' . __METHOD__);
+
+        // disabling the profiler in a production environment
+        if('production' !== $this->getEnvironment())
+        {
+            $this->bootstrap('db');
+            $profiler = new Zend_Db_Profiler_Firebug('All DB Queries');
+            $profiler->setEnabled(true);
+
+            $this->getPluginResource('db')
+                ->getAdapter()
+                ->setProfiler($profiler);
+        }
+    }
 }
